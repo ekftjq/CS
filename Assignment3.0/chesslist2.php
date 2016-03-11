66d1e8c4c1a798d678f502cd92f35a603f76b3fe
kim186 <kim186@f1011371-78be-46d3-aba7-cd4af10d11f4>
2016-03-11 17:17:48 +0000

    Assignment 3.0 Initial commit

---
+++ Assignment3.0/chesslist2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moons
+ * Date: 3/11/2016
+ * Time: 10:46 AM
+ */
+
+$xml = simplexml_load_file('svn_log.xml') or die("Error: Cannot create Object");
+
+foreach($xml->logentry as $log) {
+    $date = substr($log->date, 0, 10);
+    $time = substr($log->date, 11, 12);
+    $path = $log->paths->path;
+    $msg = $log->msg;
+    $auth = $log->author;
+    if(strpos($path, 'Assignment1.') !== false) {
+        echo "$date: submitted on $time Commit Message : <b>$msg</b><br>\n";
+    }
+
+}

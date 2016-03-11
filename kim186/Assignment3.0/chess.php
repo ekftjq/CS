f8c0a2fc3f437cd801c0817797a2f0d80999e7a7
kim186 <kim186@f1011371-78be-46d3-aba7-cd4af10d11f4>
2016-03-11 17:17:48 +0000

    Assignment 3.0 Initial commit

---
+++ kim186/Assignment3.0/chess.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : MetroBiz
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140207
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+    <link href="css/templated-metrobiz/default.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/templated-metrobiz/fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+    <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+
+<div id="wrapper3">
+    <div id="portfolio" class="container">
+
+        <div class="title">
+            <h2>Chess Project</h2>
+            <span class="byline">Feb 1 ~ Feb 19, 2016</span> </div>
+        <div class="column1">
+            <div class="box">
+                <h3>File Data</h3>
+                <p>List of files used for submission</p>
+                <a href="chesslist.php" class="button button-small">List of files</a></div>
+        </div>
+        <div class="column2">
+            <div class="box">
+                <h3>SVN Data</h3>
+                <p>List of SVN Data</p>
+                <a href="chesslist2.php" class="button button-small">Date and Time</a></div>
+        </div>
+
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
+
---
+++ kim186/Assignment3.0/hello.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moons
+ * Date: 3/11/2016
+ * Time: 8:01 AM
+ */
+
+echo "Hello World!";

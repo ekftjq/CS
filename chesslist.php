28a6ef9c1f162d321422a9556b59aabd183fa5cf
kim186 <kim186@f1011371-78be-46d3-aba7-cd4af10d11f4>
2016-03-11 17:16:14 +0000

    Assignment 3.0 Initial commit

---
+++ chesslist.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moons
+ * Date: 3/11/2016
+ * Time: 9:23 AM
+ */
+$svnlist = simplexml_load_file('svn_list.xml') or die("Error: Cannot create Object");
+
+foreach($svnlist->list->entry as $entry) {
+    $name = $entry->name;
+    if(strpos($name, 'Assignment1.') !== false) {
+        echo "$name<br>\n";
+    }
+
+}
+
+
+
---
+++ footer.php
@@ -0,0 +1,25 @@
+<div id="wrapper4">
+    <div id="footer" class="container">
+        <div>
+            <header class="title">
+                <h2>Get in touch</h2>
+                <span class="byline">Connect with Me!</span> </header>
+            <ul class="contact">
+                <li><a href="https://www.facebook.com/kimmoonseop" class="icon icon-facebook"><span></span></a></li>
+                <li><a href="https://www.linkedin.com/in/moon-seop-kim-a2a95564" class="icon icon-linkedin"><span>LinkedIn</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="copyright" class="container">
+    <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moons
+ * Date: 3/11/2016
+ * Time: 8:47 AM
+ */
+
